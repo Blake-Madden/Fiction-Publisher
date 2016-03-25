b6637ad0fd61b2ce82dc093041230e7431e7774e
Christopher Tarwater <ctarwater@gmail.com>
2016-03-25 14:44:04 -0700

    add mobi creation to bind.sh script

--- bind.sh
+++ bind.sh
@@ -11,12 +11,23 @@ done
 
 # Create epub for every book in _site
 # "${i%%.*}" gets the filename but ignores the extension
-for i in $(ls _site); do
-    pandoc --toc-depth=1 --template=Pandoc/templates/custom-epub.html --epub-stylesheet=Pandoc/css/style.css -o Books/"${i%%.*}".epub _site/$i
+for i in $(ls _site -I *Amazon*); do
+    mkdir Books/"${i%%.*}"
+    pandoc --toc-depth=1 --template=Pandoc/templates/custom-epub.html --epub-stylesheet=Pandoc/css/style.css -o Books/"${i%%.*}"/"${i%%.*}".epub _site/$i
 done
 
-# Create epub for every book in _site
+# Create mobi for every book in _site
+for i in $(ls _site | grep Amazon); do
+    pandoc --toc-depth=1 --template=Pandoc/templates/amazon-epub.html --epub-stylesheet=Pandoc/css/style.css -o Books/"${i%-Amazon.md}"/"${i%%.*}".epub _site/$i
+    
+    kindlegen -c2 Books/"${i%-Amazon.md}"/"${i%%.*}".epub
+    rm Books/"${i%-Amazon.md}"/"${i%%.*}".epub
+    
+done
+
+
+# Create PDF for every book in _site
 # "${i%%.*}" gets the filename but ignores the extension
-for i in $(ls _site); do
-    pandoc --toc --toc-depth=1 --latex-engine=xelatex -V documentclass=report -o Books/"${i%%.*}".pdf _site/$i
+for i in $(ls _site -I *Amazon*); do
+    pandoc --toc --toc-depth=1 --latex-engine=xelatex -V documentclass=report -o Books/"${i%%.*}"/"${i%%.*}".pdf _site/$i
 done
