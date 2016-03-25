ef4b048f6190fc4c7477c4d5dd4472b06386e260
Christopher Tarwater <ctarwater@gmail.com>
2016-03-25 14:04:34 -0700

    add pdf creation to bind.sh

--- bind.sh
+++ bind.sh
@@ -14,3 +14,9 @@ done
 for i in $(ls _site); do
     pandoc --toc-depth=1 --template=Pandoc/templates/custom-epub.html --epub-stylesheet=Pandoc/css/style.css -o Books/"${i%%.*}".epub _site/$i
 done
+
+# Create epub for every book in _site
+# "${i%%.*}" gets the filename but ignores the extension
+for i in $(ls _site); do
+    pandoc --toc --toc-depth=1 --latex-engine=xelatex -V documentclass=report -o Books/"${i%%.*}".pdf _site/$i
+done
